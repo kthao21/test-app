138d57d9ce3da774f910fb25770c6fcaef2da8e1
Kalia Thao <kaliathao21@gmail.com>
2024-08-16 12:55:52 -0700

    Added event handler

--- my-app/src/components/NavTabs.js
+++ my-app/src/components/NavTabs.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Body from './pages/Body';
+import Body from '../pages/Body';
 
 function NavTabs() {
     const [showBody, setShowBody] = useState(false);
